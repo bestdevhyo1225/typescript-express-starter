7bc4b3d49933f37387ed37391ecd4ef3770247e6
janghyoseog <bestdevhyo1225@gmail.com>
2019-08-28 19:44:04 +0900

    feat: Add necessary util function

---
+++ src/utils/secrets.ts
@@ -0,0 +1,23 @@
+import logger from './logger';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+if (fs.existsSync('.env')) {
+    logger.debug('Using .env file to supply config environment variables');
+    dotenv.config({ path : '.env' });
+}
+
+export const ENVIRONMENT = process.env.NODE_ENV;
+const prod = ENVIRONMENT === 'production';
+
+export const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    if (prod) {
+        logger.error('No mongo connection string. Set MONGODB_URI environment variable.');
+    } else {
+        logger.error('No mongo connection string. Set MONGODB_URI_LOCAL environment variable.');
+    }
+    process.exit(1);
+}
+
