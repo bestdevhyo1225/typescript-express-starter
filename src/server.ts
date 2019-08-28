a7f929ba684d6760cbe1051d67e8bff56748bab1
janghyoseog <bestdevhyo1225@gmail.com>
2019-08-28 20:29:40 +0900

    refactor: Modify server connect success message

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import server from './app';
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => console.log(`server is listening on ${port}`));
+server.listen(port, () => console.log(`Application Server is listening on ${port}`));
