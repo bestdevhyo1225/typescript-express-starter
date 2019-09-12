a2e509554b7afe5d56fa5fbf547f566680d55326
janghyoseog <bestdevhyo1225@gmail.com>
2019-09-12 18:53:56 +0900

    refactor: Modify mongdo db configuration

--- src/mongodb_connect.ts
+++ src/mongodb_connect.ts
@@ -1,12 +1,26 @@
 import mongoose from 'mongoose';
 
+const option = { useNewUrlParser : true, useCreateIndex : true };
+let connection: any = null;
+
+const connect = (mongoURI: string) => {
+    if (connection && mongoose.connection.readyState === 1) {
+        return Promise.resolve(connection);
+    }
+
+    return mongoose.connect(mongoURI, option).then(conn => {
+        connection = conn;
+        return connection;
+    });
+};
+
 const connectMongoDB = (mongoURI: string): void => {
-    mongoose.connect(mongoURI, { useNewUrlParser : true, useCreateIndex : true })
-            .then(() => console.log(`Successfully conntected to ${mongoURI}`))
-            .catch(error => {
-                console.error('Error connecting to mongoDB : ', error);
-                process.exit(1);
-            });
+    connect(mongoURI)
+    .then(() => console.log(`Successfully conntected to ${mongoURI}`))
+    .catch(error => {
+        console.error('Error connecting to mongoDB : ', error);
+        process.exit(1);
+    });
 };
 
 export default connectMongoDB;
